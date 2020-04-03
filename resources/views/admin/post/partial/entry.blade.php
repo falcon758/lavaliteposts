905d7a4c79bf44acb725dc4061388d451e360b40
falcon758 <falcon758@users.noreply.github.com>
2020-04-03 11:51:44 +0100

    Update entry.blade.php

--- resources/views/admin/post/partial/entry.blade.php
+++ resources/views/admin/post/partial/entry.blade.php
@@ -1,19 +1,15 @@
-            <div class='row'>
-                <div class='col-md-4 col-sm-6'>
-                       {!! Form::text('name')
-                       -> label(trans('channels::post.label.name'))
-                       -> placeholder(trans('channels::post.placeholder.name'))!!}
-                </div>
+<div class='row'>
+    <div class=col-sm-12'>
+        {!! Form::text('name')
+        -> label(trans('channels::post.label.name'))
+        -> placeholder(trans('channels::post.placeholder.name'))!!}
+    </div>
 
-                <div class='col-md-4 col-sm-6'>
-                       {!! Form::text('content')
-                       -> label(trans('channels::post.label.content'))
-                       -> placeholder(trans('channels::post.placeholder.content'))!!}
-                </div>
-
-                <div class='col-md-4 col-sm-6'>
-                       {!! Form::numeric('posts_id')
-                       -> label(trans('channels::post.label.posts_id'))
-                       -> placeholder(trans('channels::post.placeholder.posts_id'))!!}
-                </div>
-            </div>
+    <div class='col-md-12'>
+        {!! Form::textarea('content')
+        -> label(trans('channels::post.label.content'))
+        -> dataUpload(trans_url($post->getUploadURL('content')))
+        -> addClass('html-editor')
+        -> placeholder(trans('channels::post.placeholder.content'))!!}
+    </div>
+</div>
